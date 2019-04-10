6121fba1b69516e692c6db625d8ff0398a29df59
Paul Dittaro <pdittaro@gmail.com>
2019-04-10 10:38:38 -0700

    Added html download

--- races/pipelines.py
+++ races/pipelines.py
@@ -5,6 +5,7 @@ from races.items import RaceResult
 import inspect
 import pdfkit
 import os
+from urllib.request import urlopen, Request
 
 from pathlib import Path
 
@@ -19,14 +20,18 @@ class RacesPipeline(object):
         self.csv_writer = csv.DictWriter(self.file, rr_keys)
         self.csv_writer.writeheader()
 
+        self.error_log = open('output/error.log', 'w')
+
     def close_spider(self, spider):
         print("Closing File")
         self.file.close()
+        self.error_log.close()
 
     def process_item(self, item, spider):
         item['url'] = self.normalize_url(item['url'])
         item['filename'] = self.download_pdf(item)
         self.csv_writer.writerow(item)
+        print("Processed: %s" % str(item))
         return item
 
     def normalize_url(self, url):
@@ -53,9 +58,35 @@ class RacesPipeline(object):
             except FileExistsError:
                 pass
 
-            filepath = "%s%s.pdf" % (filedir, item['race'])
+            filepath = "%s%s" % (filedir, item['race'])
+            pdfpath = filepath + ".pdf"
+            htmlpath = filepath + ".html"
+            options = {'quiet': ''}
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
+            }
 
-            pdfkit.from_url(item['url'], filepath)
+            try:
+                request = Request(url=item['url'], headers=headers)
+                html = urlopen(request).read()
+                path = Path(htmlpath)
+                with path.open(mode='wb') as f:
+                    f.write(html)
+
+            except Exception as e:
+                self.error_log.write(
+                    "Error fetching html: %s\n%s\n" % (str(item), e))
+                filepath = 'Error'
+                pass
+
+            try:
+                pdfkit.from_url(item['url'], pdfpath, options)
+            except Exception as e:
+                self.error_log.write(
+                    "Error fetching pdf: %s\n%s\n" % (str(item), e))
+                filepath = 'Error'
+                pass
+
             return filepath
 
         else:
